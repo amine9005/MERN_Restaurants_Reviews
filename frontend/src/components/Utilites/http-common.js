f73537c9e552b02e4c538fa27987f3eb20c08e4c
amine9005 <amamselmitrader@gmail.com>
2023-05-27 14:36:22 +0100

    baseurl updated

--- frontend/src/components/Utilites/http-common.js
+++ frontend/src/components/Utilites/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  // baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: "  https://restaurants-finder-api.onrender.com/api/v1/restaurants",
+  
   headers: {
     "Content-type": "application/json"
   }
