f3a4306ca703ce9b3fe03d9c0ea96fc851f48081
amine9005 <amamselmitrader@gmail.com>
2023-05-27 14:21:39 +0100

    corsOptions

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins =[
+    'http://localhost:3000',
+    `https://restaurants-finder-ynyr.onrender.com/`
+]
+
+export default allowedOrigins
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
-
+import cookieParser from "cookie-parser"
+import corsOptions from "./config/corsOptions.js"
 const app = express()
 
-app.use(cors())
+
+app.use(cors(corsOptions))
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/v1/restaurants", restaurants)
