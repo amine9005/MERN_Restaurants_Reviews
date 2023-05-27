b01b8db1f3f61bd29a9f4fa83ff97bd8b7ceab19
amine9005 <amamselmitrader@gmail.com>
2023-05-27 14:49:11 +0100

    allowed Origins Updated

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins =[
-    'http://localhost:3000',
-    `https://restaurants-finder-ynyr.onrender.com/`
+    `https://restaurants-finder-ynyr.onrender.com`
 ]
 
 export default allowedOrigins
